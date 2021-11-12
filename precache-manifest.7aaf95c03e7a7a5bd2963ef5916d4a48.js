4e6c4d3cc5f2649b2ef2ab9d9f74ea5837d00b39
Marcos Estevao <dusdoom@gmail.com>
2021-11-12 04:49:37 -0300

    Updates

---
+++ precache-manifest.7aaf95c03e7a7a5bd2963ef5916d4a48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862f9a7c3cfd283f10aef86384914d6a",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "e94c96de9f9fe1c3224a",
+    "url": "/app-toot-name/static/css/main.41b1bfbd.chunk.css"
+  },
+  {
+    "revision": "24b04df69c2928117338",
+    "url": "/app-toot-name/static/js/2.4fed7fd7.chunk.js"
+  },
+  {
+    "revision": "e94c96de9f9fe1c3224a",
+    "url": "/app-toot-name/static/js/main.87b549d0.chunk.js"
+  },
+  {
+    "revision": "10f00aa2016367073b45",
+    "url": "/app-toot-name/static/js/runtime-main.fb5cccab.js"
+  },
+  {
+    "revision": "93c7d9a219b1c8d012944ff99cc4c9ce",
+    "url": "/app-toot-name/static/media/funny_guy.93c7d9a2.png"
+  }
+]);
